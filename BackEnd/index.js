b1a482fd38b9d487d02774042dd6116dff637a61
WaseemAboliel <waseem.aboliel@gmail.com>
2020-03-09 16:34:39 +0200

    change port

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use('/', require('./routes/index'));
 app.use('/api/url', require('./routes/url'));
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
