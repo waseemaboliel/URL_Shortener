7a7a787e37c645d921fb173bcec8e47683d9d45b
Waseem Aboliel <waseem.aboliel@gmail.com>
2024-01-09 00:47:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use('/api/url', require('./routes/url'));
 
 app.use('/', require('./routes/index'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
